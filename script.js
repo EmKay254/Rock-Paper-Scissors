079c68b0789c2619024466aa98f872764167517f
EmKay254 <45623287+EmKay254@users.noreply.github.com>
2025-01-13 17:09:21 +0300

    Added a prompt that takes the user choice and returns it for further processing. The users choice is limited to either Rock, Paper, or Scissors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get the computer choice
 function getComputerChoice() {
-    var computerChoice = Math.floor(Math.random() * 3) + 1; // Generates a random number between 1 and 3
+    var computerChoice = Math.floor(Math.random() * 3) + 1; // Generates a random number between 1 and 3 and rounds it off so as to get a whole number
 
     // Assigns the random number to either Rock, Paper or Scissors
     if (computerChoice === 1) {
@@ -12,4 +12,17 @@ function getComputerChoice() {
     }
 }
 
-console.log("This is a test");
+// Get the human choice and validates it based on Rock, Paper or Scissors selections
+function getHumanChoice() {
+    var humanChoice = prompt("Rock Paper Scissors?: ").toLowerCase(); //Converts the users input into lowercase
+
+    if (humanChoice === "rock") {
+        return "Rock";
+    } else if (humanChoice === "paper") {
+        return "Paper";
+    } else if (humanChoice === "scissors") {
+        return "Scissors";
+    } else {
+        return null;
+    }
+}
